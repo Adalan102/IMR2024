59d078efd2e287e461719c4ba720877bbf25fa6c
EmilCosma <109912838+EmilCosma@users.noreply.github.com>
2024-10-16 13:19:15 +0300

    Added Effects

--- VRProject/Assets/Scripts/GoalTrigger.cs
+++ VRProject/Assets/Scripts/GoalTrigger.cs
@@ -9,11 +9,13 @@ public class GoalTrigger : MonoBehaviour
     public UpdateCounter updateCounter;
     public SpawnBall spawnBall;
     public AudioSource audioClip;
+    public ParticleSystem particleSystem;
     
     void OnTriggerEnter(Collider other)
     {
         updateCounter.incrementCounter(points);
         spawnBall.respawnBall();
         audioClip.Play();
+        particleSystem.Play();
     }
 }
